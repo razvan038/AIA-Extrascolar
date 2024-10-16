f9f4c9118f11cc4fc467e6898779d0830a426ca8
Diana Mircea <mirceadiana502@gmail.com>
2024-10-16 12:31:35 +0300

    citire vector si afisare

---
+++ diana/javalab3/src/Tablou.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Tablou {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Introduceti primul numar: ");
+        int x = citesteNr();
+        System.out.println("Numarul introdus este: " + x);
+        System.out.println("Introduceti vectorul: ");
+
+        int v[] = new int[x];
+        for (int i = 0; i < v.length; i++) {
+            v[i] = citesteNr();
+        }
+
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(v[i]);
+        }
+    }
+
+    public static int citesteNr() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return Integer.parseInt(s);
+    }
+}
